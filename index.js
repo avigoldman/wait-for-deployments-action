8105b57595b1e625ccf520f8005abc6fd594000e
Avi Goldman <avrahamymgoldman@gmail.com>
2021-07-22 20:09:23 -0400

    a first pass

--- index.js
+++ index.js
@@ -1,11 +1,77 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const { get, last } = require("lodash");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+let cleanChecks = 0;
+
+while (cleanChecks < 3) {
+  if (await checkIfDeploymentsAreDone()) {
+    cleanChecks++;
+    console.log(`passed ${cleanChecks} times`);
+  } else {
+    cleanChecks = 0;
+    console.log(`Waiting again...`);
+  }
+
+  await sleep(parseInt(core.getInput("max_timeout")) * 1000);
+}
+
+setTimeout(() => {
+  core.setFailed(
+    `Timed out after ${core.getInput(
+      "max_timeout"
+    )} seconds of waiting for deployments`
+  );
+}, parseInt(core.getInput("max_timeout")) * 1000);
+
+async function checkIfDeploymentsAreDone() {
+  try {
+    const token = core.getInput("github_token");
+    const octokit = github.getOctokit(token);
+    const repoName = github.context.payload.repository.full_name;
+    const commit = github.context.payload.after;
+    const branch = last(
+      get(github, "context.payload.head.ref", get("context.payload.ref")).split(
+        "/"
+      )
+    );
+
+    const { data: commitDeployments } = await octokit.request(
+      `GET /repos/${repoName}/deployments`,
+      {
+        ref: commit,
+        per_page: 100,
+      }
+    );
+
+    const { data: branchDeployments } = await octokit.request(
+      `GET /repos/${repoName}/deployments`,
+      {
+        ref: branch,
+        per_page: 100,
+      }
+    );
+
+    const deployments = [...commitDeployments, ...branchDeployments];
+
+    for (const deployment of deployments) {
+      const { state } = await octokit.request(
+        `GET /repos/${repoName}/deployments/${deployment.id}/statuses`
+      );
+
+      if (state === "error") {
+        throw new Error(`${deployment.environment} failed.`);
+      }
+
+      if (state === "pending") {
+        return false;
+      }
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
