27cc4b7f98e5144b64e6ca61af1c884c7a9cf841
Avi Goldman <avrahamymgoldman@gmail.com>
2021-07-22 21:41:44 -0400

    test

--- index.js
+++ index.js
@@ -95,7 +95,9 @@ async function checkIfDeploymentsAreDone() {
       `GET /repos/${repoName}/deployments/${deployment.id}/statuses`
     );
 
-    console.log({ data });
+    const state = get(data, "data.0.state");
+
+    console.log({ data, state });
 
     // if (state === "failure") {
     //   throw new Error(`${deployment.environment} failed.`);
