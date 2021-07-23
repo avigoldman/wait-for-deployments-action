b37893fd3ace9bee1f6d9e957bb493d995cdf12d
Avi Goldman <avrahamymgoldman@gmail.com>
2021-07-22 21:09:15 -0400

    test

--- index.js
+++ index.js
@@ -38,14 +38,15 @@ async function checkIfDeploymentsAreDone() {
   const octokit = github.getOctokit(token);
   const repoName = github.context.payload.repository.full_name;
   const commit = github.context.payload.after;
-  console.log(JSON.stringify(github.context.payload, null, 2));
-  // const branch = last(
-  //   get(github, "context.payload.head.ref", get(github, "context.payload.ref", '')).split(
-  //     "/"
-  //   )
-  // );
+  const branch = last(
+    get(
+      github,
+      "context.payload.head.ref",
+      get(github, "context.payload.ref", "")
+    ).split("/")
+  );
 
-  // console.log({ repoName, commit, branch });
+  console.log({ repoName, commit, branch });
 
   // const { data: commitDeployments } = await octokit.request(
   //   `GET /repos/${repoName}/deployments`,
