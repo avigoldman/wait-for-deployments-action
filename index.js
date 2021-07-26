8d41f38d7831d5939312dab45a948704a4c54d21
Avi Goldman <avrahamymgoldman@gmail.com>
2021-07-26 19:05:09 -0400

    test

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ const toTime = require("to-time");
 const { get, last } = require("lodash");
 
 const REQUIRED_CHECK_COUNT = 3;
-
 // get the inital delay, max run time, and check interval
 const INITIAL_DELAY = toTime(core.getInput("initial_delay")).ms();
 const MAX_TIMEOUT = toTime(core.getInput("max_timeout")).ms();
 const CHECK_INTERVAL = toTime(core.getInput("check_interval")).ms();
-
 // Regex to filter out whether we want to wait for a deployment
 const ENVIRONMENT_REGEX = core.getInput("environment_filter")
   ? new RegExp(core.getInput("environment_filter"))
   : null;
-
 // git details
 const GIT_REPO = github.context.payload.repository.full_name;
 const GIT_COMMIT = get(
@@ -29,82 +26,89 @@ const GIT_BRANCH = last(
     get(github, "context.payload.pull_request.head.ref", "")
   ).split("/")
 );
-
 // octokit to make GitHub API calls
 const GITHUB_TOKEN = core.getInput("github_token");
 const octokit = github.getOctokit(GITHUB_TOKEN);
 
-/**
- * Fail the process after we run out of time
- */
-const timeout = sleep(MAX_TIMEOUT).then(() => {
-  core.setFailed(
-    `Timed out after ${MAX_TIMEOUT / 1000} seconds of waiting for deployments`
-  );
-});
-
-/**
- * Run the deployment checks
- */
-const runner = (async () => {
-  core.info(`Starting...`);
-  let cleanChecks = 0;
+(async () => {
+  /**
+   * Wait for initial delay
+   */
+  core.info(`Waiting inital delay of ${INITIAL_DELAY / 1000} seconds`);
+  await sleep(INITIAL_DELAY);
 
   /**
-   * We confirm 3 times in case there is a deploy that is slow to start up
+   * Fail the process after we run out of time
    */
-  let deployments;
-  while (cleanChecks < REQUIRED_CHECK_COUNT) {
-    core.info(`Running deployment check... `);
-    deployments = await getRelatedDeployments();
-    const isPending = deployments.find(({ state }) => state !== "success");
-    if (isPending) {
-      cleanChecks = 0;
-      core.info(
-        `Pending deployments. Checking again in ${
-          CHECK_INTERNVAL / 1000
-        } seconds`
-      );
-      await sleep(CHECK_INTERVAL);
-    } else {
-      cleanChecks++;
+  const timeout = sleep(MAX_TIMEOUT).then(() => {
+    core.setFailed(
+      `Timed out after ${MAX_TIMEOUT / 1000} seconds of waiting for deployments`
+    );
+  });
 
-      if (cleanChecks < REQUIRED_CHECK_COUNT) {
+  /**
+   * Run the deployment checks
+   */
+  const checker = (async () => {
+    core.info(`Starting checks...`);
+    let cleanChecks = 0;
+
+    /**
+     * We confirm 3 times in case there is a deploy that is slow to start up
+     */
+    let deployments;
+    while (cleanChecks < REQUIRED_CHECK_COUNT) {
+      core.info(`Running deployment check... `);
+      deployments = await getRelatedDeployments();
+      const isPending = deployments.find(({ state }) => state !== "success");
+      if (isPending) {
+        cleanChecks = 0;
         core.info(
-          `Passed ${cleanChecks} time${
-            cleanChecks === 1 ? "" : "s"
-          }. Waiting 30 seconds before next check...`
+          `Pending deployments. Checking again in ${
+            CHECK_INTERNVAL / 1000
+          } seconds`
         );
-        await sleep(30 * 1000);
+        await sleep(CHECK_INTERVAL);
       } else {
-        core.info(`Passed ${cleanChecks} time${cleanChecks === 1 ? "" : "s"}.`);
+        cleanChecks++;
+
+        if (cleanChecks < REQUIRED_CHECK_COUNT) {
+          core.info(
+            `Passed ${cleanChecks} time${
+              cleanChecks === 1 ? "" : "s"
+            }. Waiting 30 seconds before next check...`
+          );
+          await sleep(30 * 1000);
+        } else {
+          core.info(
+            `Passed ${cleanChecks} time${cleanChecks === 1 ? "" : "s"}.`
+          );
+        }
       }
     }
-  }
 
-  core.info(
-    `${deployments.length} deployment${
-      deployments.length === 1 ? "" : "s"
-    } look good 🚀`
-  );
-  core.setOutput("deployments", deployments);
-})();
+    core.info(
+      `${deployments.length} deployment${
+        deployments.length === 1 ? "" : "s"
+      } look good 🚀`
+    );
+    core.setOutput("deployments", deployments);
+  })();
 
-/**
- * Race the timeout and the runner
- *
- * When either finishes, kill the process.
- *
- * If either throws and error kill the process with an error.
- */
-Promise.race([timeout, runner])
-  .then(() => {
-    process.exit();
-  })
-  .catch((error) => {
+  /**
+   * Race the timeout and checker
+   *
+   * When either finishes, kill the process.
+   *
+   * If either throws and error kill the process with an error.
+   */
+  try {
+    await Promise.race([timeout, checker]);
+  } catch (error) {
     core.setFailed(error.message);
-    process.exit();
-  });
+  }
+  process.exit();
+})();
 
 /**
  * Returns an array of deployments tied to the GIT_COMMIT or
